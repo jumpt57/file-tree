befd14830901f5709c3d31e6e36145a38422d1a4
jumpt57 <julien.maupetit@protonmail.com>
2018-09-19 17:01:34 +0200

    add node class to handle same name path with index also

--- src/RenderFileTree.java
+++ src/RenderFileTree.java
@@ -1,9 +1,15 @@
 import java.util.*;
 
+import static java.lang.System.out;
 import static java.util.Objects.isNull;
 
 class RenderFileTree {
 
+    private static final int START = 0;
+    private static final String SPACE = "    ";
+    private static final String NEW_LINE = "\n";
+    private static final String SPLIT = "/";
+    private static final String DELIMITER = "";
 
     /**
      *     etc
@@ -13,11 +19,16 @@ class RenderFileTree {
      *                 website.conf
      *         passwd
      *     home
+     *         etc
+     *             hosts
+     *                 photos
+     *                     profile.jpg
      *         michel
      *             cv.pdf
      *             photos
      *                 profile.jpg
-     *                 wallpaper.jpg
+     *                 wallpaper.png
+     *
      * @param args
      */
     public static void main(String[] args) {
@@ -29,55 +40,102 @@ class RenderFileTree {
                 "/home/michel/cv.pdf",
                 "/etc/hosts",
                 "/home/michel/photos/profile.jpg",
-                "/home/michel"
+                "/home/michel",
+                "/home/etc/hosts/photos/profile.jpg"
         };
 
         renderTree(paths);
     }
 
     private static void renderTree(String[] N) {
+        Map<Node, List<Node>> nodes = createNodeTree(N);
+        Node firstNode = nodes.entrySet().iterator().next().getKey();
+        StringBuilder stringBuilder = new StringBuilder();
+        walkTree(nodes, stringBuilder, firstNode, START);
+        out.println(stringBuilder.toString());
+    }
 
-        Map<String, List<String>> paths = new LinkedHashMap<>();
+    private static Map<Node, List<Node>> createNodeTree(String[] paths) {
+        Map<Node, List<Node>> nodes = new LinkedHashMap<>();
 
-        for (String aN : N) {
-            String[] subs = aN.split("/");
+        for (String path : paths) {
+            String[] subs = path.split(SPLIT);
             for (int j = 0; j < subs.length - 1; j++) {
-                if (isNull(paths.get(subs[j]))) {
-                    List<String> newList = new ArrayList<>();
-                    paths.put(subs[j], newList);
-                    newList.add(subs[j + 1]);
+
+                int next = j + 1;
+                Node node = new Node(j, subs[j]);
+                Node child = new Node(next, subs[next]);
+
+                if (isNull(nodes.get(node))) {
+                    List<Node> children = new ArrayList<>();
+                    nodes.put(node, children);
+                    children.add(child);
                 } else {
-                    List<String> strings = paths.get(subs[j]);
-                    strings.add(subs[j + 1]);
-                    strings.sort(String::compareTo);
-                    Set<String> withoutDuplicates = new LinkedHashSet<>(strings);
-                    paths.put(subs[j], new ArrayList<>(withoutDuplicates));
+                    List<Node> children = nodes.get(node);
+                    children.add(child);
+                    children.sort(Comparator.comparing(Node::getName));
+                    Set<Node> withoutDuplicates = new LinkedHashSet<>(children);
+                    nodes.put(node, new ArrayList<>(withoutDuplicates));
                 }
             }
         }
-
-        StringBuilder stringBuilder = new StringBuilder();
-        walkTree(paths, stringBuilder, "", 1);
-        System.out.println(stringBuilder.toString());
+        return nodes;
     }
 
-    private static void walkTree(Map<String, List<String>> paths, StringBuilder builder, String next, Integer level) {
-        builder.append(String.join("", Collections.nCopies(level, "    ")));
-        builder.append(next);
-        builder.append("\n");
-        for (String sub : findNext(paths, next)) {
-            walkTree(paths, builder, sub, ++level);
+    private static void walkTree(Map<Node, List<Node>> paths, StringBuilder builder, Node next, Integer level) {
+        builder.append(String.join(DELIMITER, Collections.nCopies(level, SPACE)));
+        builder.append(next.getName());
+        builder.append(NEW_LINE);
+        for (Node node : findNext(paths, next, level)) {
+            walkTree(paths, builder, node, ++level);
             --level;
         }
     }
 
-    private static List<String> findNext(Map<String, List<String>> paths, String key) {
-        for (Map.Entry<String, List<String>> entry : paths.entrySet()) {
-            if (entry.getKey().equals(key)) {
+    private static List<Node> findNext(Map<Node, List<Node>> paths, Node node, Integer level) {
+        for (Map.Entry<Node, List<Node>> entry : paths.entrySet()) {
+            if (entry.getKey().equals(node)) {
                 return entry.getValue();
             }
         }
         return Collections.emptyList();
+    }
+
+    static class Node {
+
+        private final Integer level;
+        private final String name;
+
+        Node(Integer level, String name) {
+            this.level = level;
+            this.name = name;
+        }
+
+        String getName() {
+            return name;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Node node = (Node) o;
+            return Objects.equals(level, node.level) &&
+                    Objects.equals(name, node.name);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(level, name);
+        }
+
+        @Override
+        public String toString() {
+            return "Node{" +
+                    "level=" + level +
+                    ", name='" + name + '\'' +
+                    '}';
+        }
     }
 }
 
